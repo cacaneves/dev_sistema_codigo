7bdcc981be22ddf3be91108f6881c0269662c074
Carlos Henrique Da Silva Neves <TIJ0129137@aluno.firjansenaisesi.com.br>
2025-09-30 21:25:49 -0300

    interacao BD

--- code_com_bd.py
+++ code_com_bd.py
@@ -1,34 +1,40 @@
 from sqlalchemy import create_engine, text
 
-#Criar  e conexão com o banco 
-engine = create_engine("sqlite:///teste.db", echo = True, future=True)
 
-with engine.connect() as conn:
-    conn.execute(text("""
-                   CREATE TABLE IF NOT EXISTS alunos(
-                      id INTEGER  PRIMARY KEY AUTOINCREMENT,
-                      nome VARCHAR(50) NOT NULL,
-                      idade INTEGER NOT NUL,
-                      email VARCHAR(50) UNIQUE NOT NULL
-                      )   
-                      
-                     """ ))
-    conn.commit()
-#create_engine("mysql+engine://user:senha@localhost:3306/pizzaria")
-# inserir alunos
+# Criação e conexão com o banco
+engine = create_engine("sqlite:///teste3.db", echo = True, future = True)
+
 # criar tabela
 with engine.connect() as conn:
-    conn.execute(text("INSERT INTO alunos(nome, idade, email) " \
-                     "VALUES (: nome, idade, : email)"),
-                      [{"nome":"David", "idade":21, "mail":"davidvarao@senai.br"},
-                       {"nome":"Geovanni", "idade":23,"email":"geovannicuricica@senai.br"},
-                       {"nome":"Silvio", "idade":25,"email":"silviogaladaglobo@senai.br"}
-                      ]
-
-    )
+    conn.execute(text("""
+                    CREATE TABLE IF NOT EXISTS alunos(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome VARCHAR(50) NOT NULL,
+                        idade INTEGER NOT NULL,
+                        email VARCHAR(50) UNIQUE NOT NULL
+                      )
+                      """))
     conn.commit()
-    # consulta 1
-    with engine.connect( ) as conn:
-        resultado = conn.execute(text("SELECT * FROM alunos"))
-        for dado in resultado: 
-            print(dado.id, dado.nome, dado.idade, dado.email)
+
+# inserir alunos
+with engine.connect() as conn:
+    conn.execute(text("INSERT INTO alunos (nome, idade, email) " \
+                    "VALUES (:nome, :idade, :email)"),
+                    [ {"nome":"David", "idade":21, 
+                       "email":"davidvarao@senai.br"},
+                       {"nome":"Geovanni", "idade":23,
+                        "email":"geovannicuricica@senai.br"},
+                        {"nome":"Silvio", "idade":25,
+                         "email":"silviogaladaglobo@senai.br"}
+                    ]
+                )
+    conn.commit()
+
+# consulta 1
+with engine.connect() as conn:
+    resultado = conn.execute(text("SELECT * FROM alunos"))
+    for dado in resultado:
+        print(dado.id, dado.nome, dado.idade, dado.email)
+
+# e2 = create_engine("mysql+pymysql://
+# user:senha@localhost:3306/pizzaria")
